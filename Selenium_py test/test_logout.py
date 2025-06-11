77c161436a1fee9adf90de5ed962923b708e28f9
peach7619414 <sunnynelson402@gmail.com>
2025-06-11 13:58:32 -0400

    Delete Selenium_py test directory

--- Selenium_py test/test_logout.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-def test_logout():
-    driver = webdriver.Chrome()
-    driver.get("https://allarahealth.com/dashboard")
-    driver.find_element(By.ID, "logout-link").click()
-    assert "Login" in driver.page_source
-    driver.quit()
